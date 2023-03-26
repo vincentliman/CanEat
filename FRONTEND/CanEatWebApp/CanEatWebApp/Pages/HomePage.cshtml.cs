8705420e72c0cb6b75bd972ed5c1fc9d63e56187
JuanGau <nathanael.gauthama@binus.ac.id>
2023-03-26 22:08:58 +0700

    + Add home page

---
+++ FRONTEND/CanEatWebApp/CanEatWebApp/Pages/HomePage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CanEatWebApp.Pages
+{
+    public class HomePageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
