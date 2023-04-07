5000f7324cbd3b215ad14e78b1edda1f71d42bc8
JuanGau <nathanael.gauthama@binus.ac.id>
2023-04-07 11:52:52 +0700

    Adding dependencies, fixing file directory, figure out how to route

---
+++ CanEatFrontEnd/CanEatFrontEnd/Controllers/Customer/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CanEatFrontEnd.Controllers.Customer
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Views/Customer/Order/Index.cshtml");
+        }
+    }
+}
