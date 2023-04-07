58d6c1d5643322ef85fc84829cb89698f8f9e17f
JuanGau <nathanael.gauthama@binus.ac.id>
2023-04-07 18:57:23 +0700

    Route controller (not working)

---
+++ CanEatFrontEnd/CanEatFrontEnd/Controllers/Vendor/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CanEatFrontEnd.Controllers.Vendor
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Views/Vendor/Home/Index.cshtml");
+        }
+    }
+}
--- CanEatFrontEnd/CanEatFrontEnd/Program.cs
+++ CanEatFrontEnd/CanEatFrontEnd/Program.cs
@@ -20,8 +20,9 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "Customer_Home",
+    pattern: "Customer/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
