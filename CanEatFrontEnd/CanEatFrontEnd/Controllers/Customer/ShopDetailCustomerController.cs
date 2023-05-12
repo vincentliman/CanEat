074839306b226c6ab556021f7aac9abafa20ef5b
JuanGau <nathanael.gauthama@binus.ac.id>
2023-05-12 10:07:57 +0700

    Add all page besides admin and shop edit

---
+++ CanEatFrontEnd/CanEatFrontEnd/Controllers/Customer/ShopDetailCustomerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CanEatFrontEnd.Controllers.Customer
+{
+    public class ShopDetailCustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Views/Customer/ShopDetail/Index.cshtml");
+        }
+    }
+}
