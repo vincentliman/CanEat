4ad7b95023284bd5fc56979446b3a6d591173507
JuanGau <nathanael.gauthama@binus.ac.id>
2023-05-15 21:56:13 +0700

    Change Navbar for different role

---
+++ CanEatFrontEnd/CanEatFrontEnd/Controllers/Vendor/FoodEditController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CanEatFrontEnd.Controllers.Vendor
+{
+    public class FoodEditController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Views/Vendor/FoodEdit/Index.cshtml");
+        }
+    }
+}
--- CanEatFrontEnd/CanEatFrontEnd/Models/Customer.cs
+++ CanEatFrontEnd/CanEatFrontEnd/Models/Customer.cs
@@ -7,6 +7,6 @@
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Password { get; set; }
-        public String CompanyID { get; set; }
+        public string CompanyID { get; set; }
     }
 }
